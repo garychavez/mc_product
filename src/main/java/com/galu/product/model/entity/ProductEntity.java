bec4365d98fa82998c74c8d24b41e87ab55549f0
Gary chavez <chjalberto.96@gmail.com>
2025-10-18 16:31:41 -0500

    feat: entity layer implementation

--- src/main/java/com/galu/product/model/entity/ProductEntity.java
+++ src/main/java/com/galu/product/model/entity/ProductEntity.java
@@ -1,4 +1,22 @@
 package com.galu.product.model.entity;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GenerationType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table( name  =  "product" )
 public class ProductEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idProduct;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "description")
+    private String description;
 }
