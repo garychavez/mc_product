5df690591f4ac106cf34109f2bd26b39161e9dd6
Gary chavez <chjalberto.96@gmail.com>
2025-10-18 17:20:47 -0500

    feat: repository layer implementation

--- src/main/java/com/galu/product/repository/ProductRepository.java
+++ src/main/java/com/galu/product/repository/ProductRepository.java
@@ -1,4 +1,7 @@
 package com.galu.product.repository;
 
-public interface ProductRepository {
+import com.galu.product.model.entity.ProductEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<ProductEntity,Long> {
 }
