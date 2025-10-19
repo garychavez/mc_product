4beb26557d6c9e10424ddf9a8d9a445f20e6b922
Gary chavez <chjalberto.96@gmail.com>
2025-10-18 19:05:00 -0500

    feat: controller layer implementation

--- src/main/java/com/galu/expose/web/ProductController.java
+++ src/main/java/com/galu/expose/web/ProductController.java
@@ -1,4 +1,32 @@
 package com.galu.expose.web;
 
+import com.galu.product.model.request.ProductRequest;
+import com.galu.product.model.response.ProductResponse;
+import com.galu.product.service.ProductService;
+import io.reactivex.Completable;
+import io.reactivex.Maybe;
+import io.reactivex.Observable;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+
+@RestController
 public class ProductController {
+    @Autowired
+    ProductService productService;
+
+    @PostMapping
+    public Completable save(@RequestBody ProductRequest request){
+        return null;
+    }
+
+    @GetMapping
+    public Maybe<ProductResponse> getById(@PathVariable("idProduct") Long idProduct){
+        return null;
+    }
+
+    @GetMapping
+    public Observable<ProductResponse> getData(){
+        return null;
+    }
 }
