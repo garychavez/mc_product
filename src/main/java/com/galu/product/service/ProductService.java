e48faaec66c863dd96794d3cde1a73fa36309f3b
Gary chavez <chjalberto.96@gmail.com>
2025-10-18 19:19:15 -0500

    feat: service layer implementation

--- src/main/java/com/galu/product/service/ProductService.java
+++ src/main/java/com/galu/product/service/ProductService.java
@@ -1,4 +1,14 @@
 package com.galu.product.service;
 
+import com.galu.product.model.request.ProductRequest;
+import com.galu.product.model.response.ProductResponse;
+import io.reactivex.Completable;
+import io.reactivex.Maybe;
+import io.reactivex.Observable;
+import org.springframework.web.bind.annotation.PathVariable;
+
 public interface ProductService {
+    Completable save(ProductRequest request);
+    Maybe<ProductResponse> getById(Long idProduct);
+    Observable<ProductResponse> getData();
 }
--- src/main/java/com/galu/product/service/impl/ProductServiceImplement.java
+++ src/main/java/com/galu/product/service/impl/ProductServiceImplement.java
@@ -1,4 +1,32 @@
 package com.galu.product.service.impl;
 
-public class ProductServiceImplement {
+import com.galu.product.model.request.ProductRequest;
+import com.galu.product.model.response.ProductResponse;
+import com.galu.product.repository.ProductRepository;
+import com.galu.product.service.ProductService;
+import io.reactivex.Completable;
+import io.reactivex.Maybe;
+import io.reactivex.Observable;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProductServiceImplement implements ProductService {
+    @Autowired
+    ProductRepository repository;
+
+    @Override
+    public Completable save(ProductRequest request) {
+        return null;
+    }
+
+    @Override
+    public Maybe<ProductResponse> getById(Long idProduct) {
+        return null;
+    }
+
+    @Override
+    public Observable<ProductResponse> getData() {
+        return null;
+    }
 }
